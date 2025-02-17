0185f08aa204a719196925640f1c81fd743fa45d
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-02-17 15:53:42 +0800

    Finish part 4

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 require("dotenv").config(); // to use the .env files (node modules, etc.)
 
 const express = require("express");
-const app = express();
 const expressLayout = require("express-ejs-layouts");
 
+const connectDB = require("./server/config/db");
+
+const app = express();
 const PORT = process.env.PORT || 4000;
+
+// Database Connection
+connectDB();
 
 app.use(express.static("public"));
 
